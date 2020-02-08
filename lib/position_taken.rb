07c3b9ffac57ec4410647e242f3de24f8b07144b
Sam Tamburri <tamburrisam@gmail.com>
2020-02-08 16:34:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,18 +1,6 @@
 def position_taken? (board, index)
-  if board[index] == " "
-  return false
-  elsif board[index] == ""
-  return false 
-  elsif board[index] == nil 
-  return false
+  if !board[index] == " " || board[index] == "" || board[index] == nil 
   elsif board[index] == "X" || "O"
   return true
 end
 end
-
-
-
-  
-  
-  
-  
